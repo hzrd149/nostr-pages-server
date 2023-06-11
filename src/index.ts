2a7e4897d981f6442a1bd3f5c0c3246917bcbccb
hzrd149 <github@hzrd149.com>
2023-06-11 12:16:09 -0500

    fix bug with headers

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,28 @@ import Koa from "koa";
 import { Event, Filter, Kind, nip19 } from "nostr-tools";
 import { ensureConnected, getRelay } from "./relays.js";
 
-const app = new Koa({
-  subdomainOffset: process.env.NODE_ENV === "development" ? 1 : 2,
-});
-
-const defaultRelays = [
+const DEFAULT_PAGE =
+  "ec79a4691d70c3587ce378dbe28c10eb1e926904a903dce97d3d76d887f9b9b6";
+const DEFAULT_RELAYS = [
   "wss://relay.damus.io",
   "wss://nostr-pub.wellorder.net",
   "wss://e.nos.lol",
 ];
+
+const isHex = /[0-9a-f]{58}/i;
+enum PageKinds {
+  Root = 30051,
+  Page = 10051,
+  ChunkedPage = 10052,
+}
+
+const app = new Koa({
+  subdomainOffset: process.env.NODE_ENV === "development" ? 1 : 2,
+});
+
 async function queryRelays(
   filter: Filter,
-  relays: string[] = defaultRelays
+  relays: string[] = DEFAULT_RELAYS
 ): Promise<Event<Kind>> {
   const events = await Promise.all(
     relays.map(async (url) => {
@@ -34,27 +44,22 @@ async function getEventById(id: string, relays: string[]) {
   return event;
 }
 
-function getRelaysFromPointer(decoded: nip19.DecodeResult) {
-  switch (decoded.type) {
-    case "nevent":
-    case "naddr":
-      return decoded.data.relays.length === 0
-        ? defaultRelays
-        : decoded.data.relays;
-  }
-}
-
 async function resolveEvent(
   pointer: string
-): Promise<{ event: Event<number> | undefined; relays: string[] } | undefined> {
+): Promise<Event<number> | undefined> {
+  let relays = getRelaysForPointer(pointer);
+
+  if (pointer.match(isHex)) {
+    return await queryRelays({ ids: [pointer] }, relays);
+  }
+
   const decoded = nip19.decode(pointer);
 
-  let relays = getRelaysFromPointer(decoded);
   let event: Event;
   switch (decoded.type) {
     case "nevent":
       event = await queryRelays({ ids: [decoded.data.id] }, relays);
-      return { event, relays };
+      return event;
     case "naddr":
       event = await queryRelays(
         {
@@ -64,43 +69,78 @@ async function resolveEvent(
         },
         relays
       );
-      return { event, relays };
+      return event;
   }
 }
 
-app.use(async (ctx) => {
-  const respondWithEvent = (event: Event) => {
-    ctx.header["nostr-event"] = event.id;
-    ctx.header["nsotr-pubkey"] = event.pubkey;
+function getRelaysForPointer(pointer: string) {
+  if (pointer.match(isHex)) return DEFAULT_RELAYS;
 
-    for (const tag of event.tags) {
-      if (tag[0] === "header" && tag.length === 3) {
-        ctx.header[tag[1]] = tag[2];
-      }
-    }
+  const decoded = nip19.decode(pointer);
 
-    ctx.body = event.content;
-  };
+  switch (decoded.type) {
+    case "nevent":
+    case "naddr":
+    case "nprofile":
+      return decoded.data.relays.length === 0
+        ? DEFAULT_RELAYS
+        : decoded.data.relays;
+  }
+}
+
+function respondWithEvent(ctx: Koa.Context, event: Event) {
+  ctx.response.header["x-nostr-event"] = event.id;
+  ctx.response.header["x-nsotr-pubkey"] = event.pubkey;
 
   try {
-    const { event, relays } = await resolveEvent(ctx.subdomains[0]);
+    // try to parse base64 blob
+    const blob = Uint8Array.from(atob(event.content), (c) => c.charCodeAt(0));
+    ctx.response.body = Buffer.from(blob);
+  } catch (e) {
+    ctx.response.body = event.content;
+  }
 
+  for (const tag of event.tags) {
+    if (tag[0] === "header" && tag.length === 3) {
+      ctx.response.set(tag[1], tag[2]);
+    }
+  }
+}
+
+const rootEventCache = new Map<string, Event>();
+app.use(async (ctx) => {
+  try {
+    const pointer = ctx.subdomains[0] || DEFAULT_PAGE;
+    const relays = getRelaysForPointer(pointer);
+    const event = rootEventCache.get(pointer) || (await resolveEvent(pointer));
     if (!event) return (ctx.state = 404);
+    rootEventCache.set(pointer, event);
 
-    if (ctx.path === "/") {
-      respondWithEvent(event);
-    } else {
-      const tag = event.tags.find((t) => t[0] === "e" && t[3] === ctx.path);
-      if (!tag) {
-        ctx.status = 404;
-        ctx.body = `${ctx.path} dose not exist on ${event.id}`;
-        return;
-      }
+    // redirect to another app if its not a root page
+    if (event.kind !== PageKinds.Root) {
+      ctx.redirect(
+        `https://nostrapp.link/#${nip19.neventEncode({
+          id: event.id,
+          relays,
+        })}`
+      );
+      return;
+    }
 
+    const tag = event.tags.find((t) => t[0] === "e" && t[3] === ctx.path);
+    if (tag) {
       const subEvent = await getEventById(tag[1], relays);
       if (!subEvent) return (ctx.status = 404);
 
-      respondWithEvent(subEvent);
+      respondWithEvent(ctx, subEvent);
+      return;
+    } else if (ctx.path === "/") {
+      respondWithEvent(ctx, event);
+      return;
+    } else {
+      ctx.status = 404;
+      ctx.body = `${ctx.path} dose not exist on ${event.id}`;
+      return;
     }
   } catch (e) {
     ctx.status = 500;
